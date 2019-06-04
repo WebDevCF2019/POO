609bde5123baa53620e38b1e6262ec6b25b5dde8
mikhawa <michaeljpitz@gmail.com>
2019-06-04 13:42:38 +0200

    theuser

--- cursus/controller/publicController.php
+++ cursus/controller/publicController.php
@@ -8,3 +8,9 @@ $a = new theuser();
 echo $a->getThelogin();
 $a->setThepwd(" lulu");
 echo "<br>{$a->getThepwd()}";
+
+$b = new theuser(
+                ["idtheuser"=>"5",
+                "thelogin"=>"Stef<br>an",
+                "thepwd"=>" lulu "]
+);
--- cursus/model/theuser.php
+++ cursus/model/theuser.php
@@ -19,7 +19,7 @@ class theuser
             // login anonyme
             $this->thelogin = "Anonyme";
         }else{
-
+            // ICI
         }
     }
 
