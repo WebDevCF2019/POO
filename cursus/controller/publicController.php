1bb6e74fe701604f6ff9e17b216a480d9d8feb29
mikhawa <michaeljpitz@gmail.com>
2019-06-04 14:44:20 +0200

    exercice 1 and 2

--- cursus/controller/publicController.php
+++ cursus/controller/publicController.php
@@ -18,3 +18,34 @@ $b = new theuser(
 );
 
 var_dump($b);
+echo "<hr><h3>Exercice 1</h3>";
+
+$c = new thestudent();
+
+echo $c->getThename();
+
+
+
+$d = new thestudent(
+    ["idthestudent"=>"66",
+        "thename"=>"<p>Mic<br>ha</p>",
+        "thesurname"=>"Greg"]
+);
+
+var_dump($d);
+
+echo "<hr><h3>Exercice 2</h3>";
+
+$e = new thesection();
+
+echo $e->getThetitle();
+
+
+
+$f = new thesection(
+    ["idthesection"=>"78",
+        "thetitle"=>"<p>Mic<br>ha</p>ël",
+        "thedesc"=>"Greg aime les poulettes"]
+);
+
+var_dump($f);
